bd6338956fc99a464b689bf7d01a34e5d40a6273
Martin VN <m4rtini89@gmail.com>
2019-07-01 15:13:43 +0200

    Source control for the batch file

---
+++ DummyExtract.bat
@@ -0,0 +1,28 @@
+set CookedPCConsolePath="C:\Program Files (x86)\Steam\steamapps\common\rocketleague\TAGame\CookedPCConsole"
+set DummyFolder=C:\Projects\Unreal\RL\UPK-stuff\RL_DummyAssets
+
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\GoalExplosions -g explosion*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Boosts -g boost*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Hats -g hat*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Antennas -g antenna*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Wheels -g wheel*.upk
+
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Menu -g Menu*.upk
+
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Labs -g labs*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\ThrowbackStadium -g ThrowbackStadium*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Dropshot -g Shattershot*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Dunk_House -g Hoops*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\UrbanCentralHaunted -g Haunted_TrainStation*.upk
+
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Arc -g arc*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\ChampionField -g CS*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Mannfield -g EuroStadium*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Farmstead -g Farm*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\NeoTokyo -g NeoTokyo*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\BeckwithPark -g Park*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\DFHStadium -g Stadium*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\UrbanCentral -g TrainStation*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\AquaDome -g Underwater*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\UtopiaStadium -g UtopiaStadium*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Wasteland -g Wasteland*.upk
