0c3c6fd5f874e665a842912f315367d45fb9970c
spencerarrasmith <spencerarrasmith@gmail.com>
2021-02-27 17:03:40 -0700

    Added SaltyShores, ForbiddenTemple, and NeonFields. Re-decrypted all other maps.

--- DummyExtract.bat
+++ DummyExtract.bat
@@ -1,7 +1,9 @@
 set CookedPCConsolePath="C:\Program Files (x86)\Steam\steamapps\common\rocketleague\TAGame\CookedPCConsole"
-set DummyFolder=C:\Projects\Unreal\RL\UPK-stuff\RL_DummyAssets
+set DummyFolder=E:\RocketLeagueMapmaking\RL_DummyAssets_Mine
 
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\GoalExplosions -g explosion*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\PaintFinishes -g paintfinish*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Skins -g skin_*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Boosts -g boost*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Hats -g hat*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Antennas -g antenna*.upk
@@ -9,11 +11,15 @@ AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %
 
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Menu -g Menu*.upk
 
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\GameInfo -g gameinfo*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\GFX -g gfx_*.upk
+
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Labs -g labs*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\ThrowbackStadium -g ThrowbackStadium*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Dropshot -g Shattershot*.upk
-AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Dunk_House -g Hoops*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\DunkHouse -g Hoops*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\UrbanCentralHaunted -g Haunted_TrainStation*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Rumble -g mutators*.upk
 
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Arc -g arc*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\ChampionField -g CS*.upk
@@ -26,3 +32,6 @@ AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\AquaDome -g Underwater*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\UtopiaStadium -g UtopiaStadium*.upk
 AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\Wasteland -g Wasteland*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\SaltyShores -g Beach*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\ForbiddenTemple -g CHN*.upk
+AssetExtraction.exe --nopreload --dummy -f %CookedPCConsolePath% --dummyFolder %DummyFolder%\Maps\NeonFields -g Music*.upk
